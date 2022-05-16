086a77cc3dcb008191b1303a7e2ecc980a6d68bc
SH <shay.he3@gmail.com>
2022-05-16 22:45:09 +0300

    added json file for sites

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel shay react cesium");
+  const data = fs.readFileSync("./sites.json", "utf8");
+
+  const json0 = JSON.parse(data);
+
+  console.log(JSON.stringify(json0));
+
+  res.send(data || "Express on Vercel shay react cesium");
 });
 
 app.listen(5000, () => {
